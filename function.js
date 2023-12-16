4b029e86a1fa4feac08f844c493325cfd191152a
CharasMaster <153767653+CharasMaster@users.noreply.github.com>
2023-12-16 20:10:52 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,53 @@
+function changetext1(){
+  const button = document.getElementById("btn1").innerHTML = "Added";
+}
+function changetext2(){
+  const button = document.getElementById("btn2").innerHTML = "Added";
+}
+function changetext3(){
+  const button = document.getElementById("btn3").innerHTML = "Added";
+}
+function changetext4(){
+  const button = document.getElementById("btn4").innerHTML = "Added";
+}
+function changetext5(){
+  const button = document.getElementById("btn5").innerHTML = "Added";
+}
+function changetext6(){
+  const button = document.getElementById("btn6").innerHTML = "Added";
+}
+function changetext7(){
+  const button = document.getElementById("btn7").innerHTML = "Added";
+}
+function changetext8(){
+  const button = document.getElementById("btn8").innerHTML = "Added";
+}
+function changetext9(){
+  const button = document.getElementById("btn9").innerHTML = "Added";
+}
+function changetext10(){
+  const button = document.getElementById("btn10").innerHTML = "Added";
+}
+function changetext11(){
+  const button = document.getElementById("btn11").innerHTML = "Added";
+}
+function changetext12(){
+  const button = document.getElementById("btn12").innerHTML = "Added";
+}
+function changetext13(){
+  const button = document.getElementById("btn13").innerHTML = "Added";
+}
+function changetext14(){
+  const button = document.getElementById("btn14").innerHTML = "Added";
+}
+function changetext15(){
+  const button = document.getElementById("btn15").innerHTML = "Added";
+}
+function changetext16(){
+  const button = document.getElementById("btn16").innerHTML = "Added";
+}
+const card = document.querySelector(".card");
+
+card.addEventListener("click", function () {
+  card.classList.toggle("is-flipped");
+});
